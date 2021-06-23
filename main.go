8dd6f8a4364beeb07d1cb9bc2ea5199b116550c0
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-06-23 22:00:11 +0200

    Lesson 1 Go
    created main.go; import "fmt"
    Lesson 1.

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("GO rules!")
+}
+
+// terminal go run ... or ls - shows what is inside the folder
+// go build .\main.go -> ./main.exe will run the built file
