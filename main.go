7e74d70c18b66d156a8f9d03b5e0c0e26f7cb791
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-06-24 16:06:09 +0200

    lesson 2 in go website
    made 2 pages and connected to localhost, start/stop server to see the contents

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+func home_page(page http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(page, "Go is super easy!")
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts page text!")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil) //free port on your PC after comma server parameters, sending empty - nil
+
+}
 func main() {
-	fmt.Println("GO rules!")
+	handleRequest()
+
 }
 
 // terminal go run ... or ls - shows what is inside the folder
 // go build .\main.go -> ./main.exe will run the built file
+//ctrl+c -> stop the server
